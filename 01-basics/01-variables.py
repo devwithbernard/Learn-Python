aa602ed13f86d9320d9511cb3dc4b02086c43083
Bernard Konan <devwithbernard@gmail.com>
2025-06-29 07:20:24 +0000

    Write documentation for learning Variables in python

---
+++ 01-basics/01-variables.py
@@ -0,0 +1,14 @@
+"""
+Title: Python Variables - Introduction
+
+Description:
+This script introduces the concept of variables in Python. It covers:
+- What a variable is
+- How to assign values to variables
+- Naming conventions
+- Multiple assignments
+- Variable types and dynamic typing
+
+Author: [Your Name]
+Date: [Date]
+"""
