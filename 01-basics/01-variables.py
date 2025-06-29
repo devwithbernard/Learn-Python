b570c16830754bacdd9996f8e66b05a31795ca3d
Bernard Konan <devwithbernard@gmail.com>
2025-06-29 07:37:47 +0000

    Assign multiple variables and print their values

--- 01-basics/01-variables.py
+++ 01-basics/01-variables.py
@@ -12,3 +12,28 @@ This script introduces the concept of variables in Python. It covers:
 Author: [Your Name]
 Date: [Date]
 """
+
+# Assign a variable
+name: str = "John Doe"
+age: int = 28
+
+print(f"Your name is {name}.\nAnd you have {age} year{'s' if age > 1 else ''} old.")
+
+product_name: str = "Renault 200"
+product_brand: str = "Renault"
+product_price: float = 10_000_000.25
+product_available: bool = True
+
+message: str
+
+if product_available:
+    message = f"""
+    Product (🚗)
+        Name: {product_name}
+        Price: ${product_price:.2f}
+        Brand: {product_brand}
+    """
+else:
+    message = "🔴Product not available."
+
+print(message)
