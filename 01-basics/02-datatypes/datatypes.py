795911ca812085cabb469c7917a1e0c5c56ad4ff
Bernard Konan <devwithbernard@gmail.com>
2025-07-01 05:30:18 +0000

    Datatypes: Print the type of sequences (list, tuple, range)

--- 01-basics/02-datatypes/datatypes.py
+++ 01-basics/02-datatypes/datatypes.py
@@ -25,3 +25,33 @@ The type of '{product_price}' is '{type(product_price).__name__}'. And
 The type of {product_quantity} is '{type(product_quantity).__name__}.'
 """)
 
+"""
+Sequence: List, Tuple, Range
+"""
+# List
+names: list[str] = ['Luigi', 'Jane', 'Mario']
+
+if isinstance(names, list):
+    print(f"Type of names: '{type(names).__name__}'")
+    for index, name in enumerate(names):
+        print(f"{index} => {name}")
+
+
+# Tuple
+persons: list[tuple[str, int]] = [('Mario', 28,), ('Luigi', 15,), ('John Doe', 47,)]
+
+for person in persons:
+    if isinstance(person, tuple):
+        name, age = person
+        print(f"name = {name}, age = {age} year{'s' if age > 1 else ''} old")
+    else:
+        print("You're not a valid person.")
+
+# Range
+numbers = range(100)
+
+print(type(numbers))
+
+for number in numbers:
+    if number % 3 == 0 and number % 5 == 0:
+        print(number)
